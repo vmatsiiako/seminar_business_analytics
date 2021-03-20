35fc270a250a9fb27443d55dd06fbe8324ff1c83
anoukveltman <anoukveltman98@gmail.com>
2021-03-20 14:56:54 +0100

    kmeans alg

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,4 +1,15 @@
+import numpy as np
+import cv2
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+# load in the data
+df = pd.read_csv("../Data/sign_mnist_train.csv")
+features = df.columns[1:]
+X_train = df.loc[:, features].values
+y = df.loc[:,['label']].values
 
 
-
-sklearn.cluster.KMeans(n_clusters=8, *, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='deprecated', verbose=0, random_state=None, copy_x=True, n_jobs='deprecated', algorithm='auto')
+sklearn.cluster.KMeans(n_clusters=8, *, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='deprecated', verbose=0, random_state=None, copy_x=True, n_jobs='deprecated', algorithm='auto').fit(x)
