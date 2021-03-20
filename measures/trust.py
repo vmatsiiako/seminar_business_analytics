1330ccd8df978b41fc02df8f41bd65f820c2475b
LZwiers <lczwiers@outlook.com>
2021-03-20 14:57:24 +0100

    changed data

--- measures/trust.py
+++ measures/trust.py
@@ -3,19 +3,27 @@ from sklearn.manifold import trustworthiness
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import cv2
 
-df = pd.read_csv("/Users/anoukveltman/Downloads/archive/sign_mnist_train.csv")
+df = pd.read_csv("../Data/sign_mnist_train.csv")
 
 features = df.columns[1:]
 X_train = df.loc[:, features].values
 
+# increase the contract of pictures
+X_contrast = np.zeros(np.shape(X_train))
+for i in range(len(X_contrast)):
+    image = X_train[i, :]
+    image = image.astype(np.uint8)
+    X_contrast[i] = cv2.equalizeHist(image).reshape(1, 784)
+
 pca = PCA(n_components=5)
 #x = df.loc[:, features].values
-x = StandardScaler().fit_transform(X_train)
+x = StandardScaler().fit_transform(X_contrast)
 princa = pca.fit_transform(x)
 
-trust = trustworthiness(X_train, princa, n_neighbors=10)
+trust = trustworthiness(X_contrast, princa, n_neighbors=10)
 #print(trust)
 #print(features)
 #print(x_train)
-print(X_train)
+print(trust)
