6cd015a86a60e3b02f713413006cb9c6803ebcff
LucaDiga <luca.zampierin@hotmail.com>
2021-03-20 12:38:22 +0100

    commented out two lines

--- Autoencoders/train.py
+++ Autoencoders/train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
+#import matplotlib
+#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
