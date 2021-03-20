83690347d1a847cf2c1f98940803b38d999ed177
anoukveltman <anoukveltman98@gmail.com>
2021-03-20 14:28:54 +0100

    extra files

---
+++ measures/conti1.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.manifold import trustworthiness
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import distance
+from trustcont import *
+
+df = pd.read_csv("../Data/sign_mnist_train.csv")
+
+features = df.columns[1:]
+X = df.loc[:, features].values
+pca = PCA(n_components=5)
+x = df.loc[:, features].values
+x = StandardScaler().fit_transform(x)
+princa = pca.fit_transform(x)
+
+ks = vector_row = np.array([10])
+
+cont = continuity(x, princa, ks)
+print(cont)
+
