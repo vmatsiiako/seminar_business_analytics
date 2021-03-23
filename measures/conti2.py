2452531f2b45a7d2dcd7a5f5098c93a794d8b540
anoukveltman <anoukveltman98@gmail.com>
2021-03-23 08:25:44 +0100

    link added

--- measures/conti2.py
+++ measures/conti2.py
@@ -7,16 +7,27 @@ import coranking
 from coranking.metrics import continuity
 df = pd.read_csv("../Data/sign_mnist_train.csv")
 
+#https://coranking.readthedocs.io/en/latest/
+
 features = df.columns[1:]
-X = df.loc[:, features].values
-pca = PCA(n_components=5)
+#X = df.loc[:, features].values
+pca = PCA(n_components=3)
 x = df.loc[:, features].values
 x = StandardScaler().fit_transform(x)
 princa = pca.fit_transform(x)
 
-Q = coranking.coranking_matrix(df, princa)
+df2 = pd.read_csv("../Data/sign_mnist_train.csv")
+
+features = df2.columns[1:]
+#X2 = df2.loc[:, features].values
+pca2 = PCA(n_components=10)
+x2 = df2.loc[:, features].values
+x2 = StandardScaler().fit_transform(x2)
+princa2 = pca2.fit_transform(x2)
+
+Q = coranking.coranking_matrix(princa2, princa)
 
 trust_pca = trustworthiness(Q, min_k=10, max_k=20)
-#cont_pca = continuity(Q, min_k=10, max_k=20)
+cont_pca = continuity(Q, min_k=10, max_k=20)
 print(trust_pca)
-#print(cont_pca)
+print(cont_pca)
