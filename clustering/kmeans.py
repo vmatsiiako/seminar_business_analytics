1af54b0dd8a3c23e1d9209a5646d2046600341e2
LZwiers <lczwiers@outlook.com>
2021-03-23 09:34:43 +0100

    added line for full dataset kmeans

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -33,36 +33,39 @@ kmeans = KMeans(init="k-means++", n_clusters=24, n_init=4)
 kmeans_pca = kmeans.fit(princa)
 print('KMeans PCA Scaled Silhouette Score: {}'.format(silhouette_score(df_scale, kmeans_pca.labels_, metric='euclidean')))
 
+kmeans_full = kmeans.fit(x)
+print('KMeans full Scaled Silhouette Score: {}'.format(silhouette_score(df_scale, kmeans_full.labels_, metric='euclidean')))
+
 h = .02
 
 # Plot the decision boundary. For that, we will assign a color to each
-x_min, x_max = princa[:, 0].min() - 1, princa[:, 0].max() + 1
-y_min, y_max = princa[:, 1].min() - 1, princa[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+#x_min, x_max = princa[:, 0].min() - 1, princa[:, 0].max() + 1
+#y_min, y_max = princa[:, 1].min() - 1, princa[:, 1].max() + 1
+#xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
 # Obtain labels for each point in mesh. Use last trained model.
-Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+#Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
 
 # Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure(1)
-plt.clf()
-plt.imshow(Z, interpolation="nearest",
-           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-           cmap=plt.cm.Paired, aspect="auto", origin="lower")
+#Z = Z.reshape(xx.shape)
+# plt.figure(1)
+# plt.clf()
+# plt.imshow(Z, interpolation="nearest",
+#            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+#            cmap=plt.cm.Paired, aspect="auto", origin="lower")
 
-plt.plot(princa[:, 0], princa[:, 1], 'k.', markersize=2)
-# Plot the centroids as a white X
-centroids = kmeans.cluster_centers_
-plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=169, linewidths=3,
-            color="w", zorder=10)
-plt.title("K-means clustering on the digits dataset (PCA-reduced data)\n"
-          "Centroids are marked with white cross")
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-plt.show()
+# plt.plot(princa[:, 0], princa[:, 1], 'k.', markersize=2)
+# # Plot the centroids as a white X
+# centroids = kmeans.cluster_centers_
+# plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=169, linewidths=3,
+#             color="w", zorder=10)
+# plt.title("K-means clustering on the digits dataset (PCA-reduced data)\n"
+#           "Centroids are marked with white cross")
+# plt.xlim(x_min, x_max)
+# plt.ylim(y_min, y_max)
+# plt.xticks(())
+# plt.yticks(())
+# plt.show()
 
 #N_clusters = 25
 
