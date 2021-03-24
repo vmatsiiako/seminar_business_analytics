c42818d562a262a534b2e38b440e00d40b223ea4
LZwiers <lczwiers@outlook.com>
2021-03-24 08:06:03 +0100

    Changed code to contrasted data

--- measures/conti2.py
+++ measures/conti2.py
@@ -5,27 +5,33 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import coranking
 from coranking.metrics import continuity
-df = pd.read_csv("../Data/sign_mnist_train.csv")
 
 #https://coranking.readthedocs.io/en/latest/
 
-features = df.columns[1:]
+df = pd.read_csv("../Data/sign_mnist_train.csv")
+X_train = df.iloc[:,1:].values
+y_train = df.iloc[:,0].values
+
+# increase the contrast of pictures
+X_contrast = np.zeros(np.shape(X_train))
+for i in range(len(X_contrast)):
+    image = X_train[i, :]
+    image = image.astype(np.uint8)
+    X_contrast[i] = cv2.equalizeHist(image).reshape(1, NUMBER_OF_PIXELS)
+
+# normalize data
+X_contrast = X_contrast.astype('float32') / MAX_BRIGHTNESS - MEAN
+X_train = X_train.astype('float32') / MAX_BRIGHTNESS - MEAN
+
+#features = df.columns[1:]
 #X = df.loc[:, features].values
+
+#run PCA with n=... principal components
 pca = PCA(n_components=3)
-x = df.loc[:, features].values
-x = StandardScaler().fit_transform(x)
-princa = pca.fit_transform(x)
+princa = pca.fit_transform(X_contrast)
 
-df2 = pd.read_csv("../Data/sign_mnist_train.csv")
-
-features = df2.columns[1:]
-#X2 = df2.loc[:, features].values
-pca2 = PCA(n_components=10)
-x2 = df2.loc[:, features].values
-x2 = StandardScaler().fit_transform(x2)
-princa2 = pca2.fit_transform(x2)
-
-Q = coranking.coranking_matrix(princa2, princa)
+#Q = coranking.coranking_matrix(high_data, low_data)
+Q = coranking.coranking_matrix(X_contrast, princa)
 
 trust_pca = trustworthiness(Q, min_k=10, max_k=20)
 cont_pca = continuity(Q, min_k=10, max_k=20)
