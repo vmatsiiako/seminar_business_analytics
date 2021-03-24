e85b902b933a3de2dfdb6c4bbdd229a01e3eb7de
LZwiers <lczwiers@outlook.com>
2021-03-24 10:54:18 +0100

    k-means with measures and clusters-labels comparison

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,42 +1,59 @@
+import pandas
 import pandas as pd
 import matplotlib
-from sklearn.metrics import silhouette_score
-
+from sklearn.metrics import silhouette_score, homogeneity_score, completeness_score, v_measure_score
+import cv2
 matplotlib.use('TkAgg')
 #https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html
 #https://towardsdatascience.com/explaining-k-means-clustering-5298dc47bad6
-
 from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-# load in the data
 df = pd.read_csv("../Data/sign_mnist_train.csv")
-features = df.columns[1:]
-X_train = df.loc[:, features].values
-y = df.loc[:,['label']].values
+X_train = df.iloc[:,1:].values
+y_train = df.iloc[:,0].values
 
-pca = PCA(n_components=2)
-#x = df.loc[:, features].values
-x = StandardScaler().fit_transform(X_train)
-princa = pca.fit_transform(x)
+#contrast data
+X_contrast = np.zeros(np.shape(X_train))
+for i in range(len(X_contrast)):
+    image = X_train[i,:]
+    image = image.astype(np.uint8)
+    X_contrast[i] = cv2.equalizeHist(image).reshape(1,784)
 
-scaler = StandardScaler()
-scaler.fit(df)
-X_scale = scaler.transform(df)
-df_scale = pd.DataFrame(X_scale, columns=df.columns)
-df_scale.head()
+# normalize data
+X_contrast = X_contrast.astype('float32') / 255.0 - 0.5
+X_train = X_train.astype('float32') / 255.0 - 0.5
+
+#run PCA with n=13 principal components
+pca = PCA(n_components=13)
+princa = pca.fit_transform(X_contrast)
 
 kmeans = KMeans(init="k-means++", n_clusters=24, n_init=4)
-kmeans_pca = kmeans.fit(princa)
-print('KMeans PCA Scaled Silhouette Score: {}'.format(silhouette_score(df_scale, kmeans_pca.labels_, metric='euclidean')))
+kmeans_full = kmeans.fit(X_contrast)
+labels = kmeans.predict(X_contrast)
 
-kmeans_full = kmeans.fit(x)
-print('KMeans full Scaled Silhouette Score: {}'.format(silhouette_score(df_scale, kmeans_full.labels_, metric='euclidean')))
+homo = homogeneity_score(y_train, labels)
+print(homo)
+comp = completeness_score(y_train, labels)
+print(comp)
+v = v_measure_score(y_train, labels)
+print(v)
 
-h = .02
+cluslabs = list(zip(y_train,labels))
+#print(cluslabs)
+cluslabdf = pandas.DataFrame(data=cluslabs)
+#cluslabdf.to_csv("cluslabs.csv", sep=',',index=False)
+
+#kmeans_pca = kmeans.fit(princa)
+#print('KMeans PCA Scaled Silhouette Score: {}'.format(silhouette_score(df_scale, kmeans_pca.labels_, metric='euclidean')))
+
+#kmeans_full = kmeans.fit(x)
+#print('KMeans full Scaled Silhouette Score: {}'.format(silhouette_score(df_scale, kmeans_full.labels_, metric='euclidean')))
+
+#h = .02
 
 # Plot the decision boundary. For that, we will assign a color to each
 #x_min, x_max = princa[:, 0].min() - 1, princa[:, 0].max() + 1
