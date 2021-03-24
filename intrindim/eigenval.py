679254fa9b01c26771862c4eff033f2b7264e705
anoukveltman <anoukveltman98@gmail.com>
2021-03-24 09:32:44 +0100

    ID

--- intrindim/eigenval.py
+++ intrindim/eigenval.py
@@ -18,9 +18,11 @@ for i in range(len(X_contrast)):
 
 X_contrast = X_contrast.astype('float32') / 255.0 - 0.5
 
-CD = skdim.id.CorrInt(k1=10, k2=40, DM=False).fit_predict(X_train)
-EigValue = skdim.id.lPCA(ver='FO', alphaRatio=0.1, alphaFO=0.1, verbose=False, fit_explained_variance=False).fit_predict(X_train)
+CD1 = skdim.id.CorrInt(k1=30, k2=50, DM=False).fit_predict(X_contrast)
+CD2 = skdim.id.CorrInt(k1=50, k2=80, DM=False).fit_predict(X_contrast)
+#EigValue = skdim.id.lPCA(ver='FO', alphaRatio=0.1, alphaFO=0.1, verbose=False, fit_explained_variance=False).fit_predict(X_contrast)
 #MLE = skdim.id.MLE(dnoise=None, sigma=0, n=None, integral_approximation='Haro', unbiased=False, neighborhood_based=True, K=10).fit_predict(x)
-print(EigValue)
-print(CD)
+#print(EigValue)
+print(CD1)
+print(CD2)
 #print(MLE)
