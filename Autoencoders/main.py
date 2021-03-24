ea782113810b0928dc8a570acb6466c069f494e6
Vladyslav Matsiiako <matsiiako@gmail.com>
2021-03-24 10:39:25 +0100

    commented matplotlib back

--- Autoencoders/main.py
+++ Autoencoders/main.py
@@ -2,8 +2,8 @@ import random
 
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
+# import matplotlib
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
