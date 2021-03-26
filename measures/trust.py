28aebca06a16b27339f968ad4becd7eb324e5a76
anoukveltman <anoukveltman98@gmail.com>
2021-03-26 09:25:48 +0100

    array added

--- measures/trust.py
+++ measures/trust.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import cv2
 
-#https://scikit-learn.org/stable/modules/generated/sklearn.manifold.trustworthiness.html
+# https://scikit-learn.org/stable/modules/generated/sklearn.manifold.trustworthiness.html
 df = pd.read_csv("../Data/sign_mnist_train.csv")
 
 features = df.columns[1:]
@@ -19,18 +19,19 @@ for i in range(len(X_contrast)):
     X_contrast[i] = cv2.equalizeHist(image).reshape(1, 784)
 
 pca = PCA(n_components=13)
-#x = df.loc[:, features].values
+# x = df.loc[:, features].values
 x = StandardScaler().fit_transform(X_contrast)
 princa = pca.fit_transform(x)
 
-trust_array = []
-for i in range(1,26):
-    trust = trustworthiness(X_contrast, princa, n_neighbors = i)
+trust_array = np.zeros(26)
+for i in range(1, 26):
+    trust = trustworthiness(X_contrast, princa, n_neighbors=i)
     trust_array[i - 1] = trust
 
-#trust = trustworthiness(X_contrast, princa, n_neighbors=10)
-#print(trust)
-#print(features)
-#print(x_train)
-#print(trust)
+
+#trust2 = trustworthiness(X_contrast, princa, n_neighbors=10)
+# print(trust)
+# print(features)
+# print(x_train)
+#print(trust2)
 print(trust_array)
