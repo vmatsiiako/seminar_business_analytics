415a7e1ec1585213769fe8a44ea23ad18da5552a
anoukveltman <anoukveltman98@gmail.com>
2021-03-29 17:48:38 +0200

    ae long name

--- measures/conti2.py
+++ measures/conti2.py
@@ -23,7 +23,7 @@ X_test = df_test.iloc[:,1:].values
 y_test = df_test.iloc[:,0].values
 
 #load in embedding from autoencoders on test data
-df_test_ae = pd.read_csv("../Data/reduced_test_set_with_BATCH_SIZE_64_NOISE_TYPE_gaussian_NOISE_PERCENTAGE_2_HIDDEN_LAYERS_[620,330,100,13]_LEATNING_RATE_0,002_EPOCH_70.csv.csv", header=None)
+df_test_ae = pd.read_csv("../Data/reduced_test_set_with_BATCH_SIZE_64_NOISE_TYPE_gaussian_NOISE_PERCENTAGE_2_HIDDEN_LAYERS_[620,330,100,13]_LEATNING_RATE_0,002_EPOCH_70.csv", header=None)
 X_test_ae = df_test_ae.iloc[:,0:].values
 
 #contrast train data
