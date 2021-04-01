16c405ec7b41593d3def3ea6832ea81200832585
LZwiers <lczwiers@outlook.com>
2021-04-01 14:06:40 +0200

    started code cleanup

--- TSNE/tsne.py
+++ TSNE/tsne.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import cv2
 import seaborn as sns
 
-# https://scikit-learn.org/stable/modules/generated/sklearn.manifold.trustworthiness.html
 df = pd.read_csv("../Data/sign_mnist_train.csv")
 
 features = df.columns[1:]
@@ -38,10 +37,6 @@ TSNE_output = TSNE.fit_transform(X_train_ae)
 #n_iter =TSNE_output.n_iter_
 #print(n_iter)
 
-#run tsne on full data
-#TSNE = TSNE(n_components=2, perplexity=5)
-#TSNE_output = TSNE.fit_transform(X_contrast)
-
 tsneDf = pd.DataFrame(data = TSNE_output)
 finalDf = pd.concat([tsneDf, df[['label']]], axis=1)
 
@@ -49,9 +44,9 @@ finalDf['tsne1'] = finalDf.loc[:,0].values
 finalDf['tsne2'] = finalDf.loc[:,1].values
 
 plt.figure(figsize=(12,8))
-plt.title("Tsne with 2 components")
+plt.title("2-component t-SNE")
 sns.scatterplot(
-    x="tsne1", y="tsne2",
+    x="component 1", y="component 2",
     hue = y_train,
     palette=sns.color_palette("hls", 24),
     data=finalDf,
