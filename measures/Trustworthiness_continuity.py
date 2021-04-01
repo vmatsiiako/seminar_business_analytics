4c173b25b10a2f88a961eabde480c432ea9a2a84
LZwiers <lczwiers@outlook.com>
2021-04-01 14:19:26 +0200

    started code cleanup

--- measures/Trustworthiness_continuity.py
+++ measures/Trustworthiness_continuity.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-from sklearn.manifold import TSNE
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
@@ -53,114 +52,89 @@ pca = PCA(n_components=13)
 princa = pca.fit_transform(X_contrast)
 princa_test = pca.fit_transform(X_contrast_test)
 
-#run tsne on full data
-#TSNE = TSNE(n_components=2, perplexity=40)
-#TSNE_output = TSNE.fit_transform(X_contrast)
-
 #pick random subsample to calculate the measures for for the train data
+#we first create numpy arrays with the original data and embeddings together
 new_data = np.hstack((X_contrast, princa))
 new_data_ae = np.hstack((X_contrast, X_train_ae))
 n_train = new_data.shape[0]
+
+#fix a random seed and get a list of random indices with size equal to half the train data size
 np.random.seed(0)
 random_indices = np.random.choice(n_train, size=13727, replace=False)
+
+#get a random sample of both numpy arrays and split them in two again
 random_sample = new_data[random_indices, :]
 full_random = random_sample[:,13:]
 pca_random = random_sample[:,:12]
 random_ae_sample = new_data_ae[random_indices, :]
 ae_random = random_ae_sample[:, :12]
 
-#pick random subsample to calculate the measures for for TSNE
-# new_data = np.hstack((X_contrast, TSNE_output))
-# number_of_rows = new_data.shape[0]
-# random_indices = np.random.choice(number_of_rows, size=100, replace=False)
-# random_sample = new_data[random_indices, :]
-# full_random_tsne = random_sample[:,2:]
-# tsne_random = random_sample[:,:1]
-
-#Q = coranking.coranking_matrix(high_data, low_data)
 #calculate coranking matrices
 Q = coranking.coranking_matrix(full_random, pca_random)
 Q_test = coranking.coranking_matrix(X_contrast_test, princa_test)
 Q_ae = coranking.coranking_matrix(full_random, ae_random)
 Q_ae_test = coranking.coranking_matrix(X_contrast_test, X_test_ae)
 
-#Q_tsne = coranking.coranking_matrix(full_random_tsne, tsne_random)
-
-#measures for PCA on train data
+#calculate and print measures for PCA on train data
 trust_pca = trustworthiness(Q, min_k=1, max_k=25)
 cont_pca = continuity(Q, min_k=1, max_k=25)
 print(trust_pca)
 print(cont_pca)
 
-#measures for pca on test data
+#calculate and print measures for pca on test data
 trust_pca_test = trustworthiness(Q_test, min_k = 1, max_k = 25)
 cont_pca_test = continuity(Q_test, min_k=1, max_k=25)
 print(trust_pca_test)
 print(cont_pca_test)
 #
-#measures for ae on train data
+#calculate and print measures for ae on train data
 trust_ae = trustworthiness(Q_ae, min_k=1, max_k=25)
 cont_ae = continuity(Q_ae, min_k=1, max_k=25)
 print(trust_ae)
 print(cont_ae)
 
-#measures for ae on test data
+#calculate and print measures for ae on test data
 trust_ae_test = trustworthiness(Q_ae_test, min_k=1, max_k=25)
 cont_ae_test = continuity(Q_ae_test, min_k=1, max_k=25)
 print(trust_ae_test)
 print(cont_ae_test)
 
-#measures for tsne
-# trust_tsne = trustworthiness(Q_tsne, min_k=1, max_k=25)
-# cont_tsne = continuity(Q_tsne, min_k=1, max_k=25)
-# print(trust_tsne)
-# print(cont_tsne)
-
-#plotting pca on train data
+#plot measures for pca on train data
 plt.plot(cont_pca, "-m", label="continuity measure")
 plt.plot(trust_pca, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
 plt.xlabel('Number of Neighbors')
 plt.ylabel('Measure')
-plt.title('pca train data')
+plt.title('Trustworthiness and continuity for PCA on training data')
 plt.savefig('pca_train_data.png')
 plt.show()
 
-#plotting pca on test data
+#plot measures for pca on test data
 plt.plot(cont_pca_test, "-m", label="continuity measure")
 plt.plot(trust_pca_test, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
 plt.xlabel('Number of Neighbors')
 plt.ylabel('Measure')
-plt.title('pca test data')
+plt.title('Trustworthiness and continuity for PCA on test data')
 plt.savefig('pca_test_data.png')
 plt.show()
 
-#plotting ae on train data
+#plot measures for AE on train data
 plt.plot(cont_ae, "-m", label="continuity measure")
 plt.plot(trust_ae, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
 plt.xlabel('Number of Neighbors')
 plt.ylabel('Measure')
-plt.title('ae train data')
+plt.title('Trustworthiness and continuity for autoencoders on training data')
 plt.savefig('ae_train_data.png')
 plt.show()
 
-#plotting ae on test data
+#plot measures for AE on test data
 plt.plot(cont_ae_test, "-m", label="continuity measure")
 plt.plot(trust_ae_test, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
 plt.xlabel('Number of Neighbors')
 plt.ylabel('Measure')
-plt.title('ae test data')
+plt.title('Trustworthiness and continuity for autoencoders on test data')
 plt.savefig('ae_test_data.png')
 plt.show()
-
-#plotting tsne
-# plt.plot(cont_tsne, "-m", label="continuity measure")
-# plt.plot(trust_tsne, "-c", label="trustworthiness measure")
-# plt.legend(loc="upper right")
-# plt.xlabel('Number of Neighbors')
-# plt.ylabel('Measure')
-# plt.set_window_title('Tsne')
-# plt.show()
