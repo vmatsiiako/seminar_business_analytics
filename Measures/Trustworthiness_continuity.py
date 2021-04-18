cfa51a34699ca962ef82c6cbabcb27f915508f9d
Vladyslav Matsiiako <matsiiako@gmail.com>
2021-04-18 12:21:11 +0200

    capitalized names

--- Measures/Trustworthiness_continuity.py
+++ Measures/Trustworthiness_continuity.py
@@ -64,7 +64,7 @@ pca = PCA(n_components=13)
 princa = pca.fit_transform(X_contrast)
 princa_test = pca.fit_transform(X_contrast_test)
 
-#pick random subsample to calculate the measures for for the train data
+#pick random subsample to calculate the Measures for for the train data
 #we first create numpy arrays with the original data and embeddings together
 new_data = np.hstack((X_contrast, princa))
 new_data_ae_deep = np.hstack((X_contrast, X_train_ae_deep))
@@ -79,17 +79,17 @@ random_indices = np.random.choice(n_train, size=13727, replace=False)
 random_indices_ae = np.random.choice(n_train_ae_deep, size=13727, replace=False)
 random_indices_ae_denoised = np.random.choice(n_train_ae_denoised, size=13727, replace=False)
 
-#create the random subsample for the pca measures
+#create the random subsample for the pca Measures
 random_sample = new_data[random_indices, :]
 full_random = random_sample[:,13:]
 pca_random = random_sample[:,:12]
 
-#create the random subsample for the deep autoencoders measures
+#create the random subsample for the deep autoencoders Measures
 random_ae_sample = new_data_ae_deep[random_indices_ae, :]
 full_random_2 = random_ae_sample[:,13:]
 ae_random = random_ae_sample[:, :12]
 
-#create the random subsample for the denoised autoencoders measures
+#create the random subsample for the denoised autoencoders Measures
 random_ae_sample_denoised = new_data_ae_denoised[random_indices_ae_denoised, :]
 full_random_3 = random_ae_sample_denoised[:,13:]
 ae_random_denoised = random_ae_sample_denoised[:, :12]
@@ -102,43 +102,43 @@ Q_ae_test = coranking.coranking_matrix(X_contrast_test, X_test_ae_deep)
 Q_ae_denoised = coranking.coranking_matrix(full_random_3, ae_random_denoised)
 Q_ae_test_denoised = coranking.coranking_matrix(X_contrast_test, X_test_ae_denoised)
 
-#calculate and print measures for PCA on train data
+#calculate and print Measures for PCA on train data
 trust_pca = trustworthiness(Q, min_k=1, max_k=25)
 cont_pca = continuity(Q, min_k=1, max_k=25)
 print('Trustworthiness measure PCA train data set{}'. format(trust_pca))
 print('Continuity measure measure PCA train data set{}'. format(cont_pca))
 
-#calculate and print measures for pca on test data
+#calculate and print Measures for pca on test data
 trust_pca_test = trustworthiness(Q_test, min_k = 1, max_k = 25)
 cont_pca_test = continuity(Q_test, min_k=1, max_k=25)
 print('Trustworthiness measure PCA test data set{}'. format(trust_pca_test))
 print('Continuity measure PCA test data set{}'. format(cont_pca_test))
 
-#calculate and print measures for deep ae on train data
+#calculate and print Measures for deep ae on train data
 trust_ae = trustworthiness(Q_ae, min_k=1, max_k=25)
 cont_ae = continuity(Q_ae, min_k=1, max_k=25)
 print('Trustworthiness measure deep AE train data set{}'. format(trust_ae))
 print('Continuity measure deep AE train data set{}'. format(cont_ae))
 
-#calculate and print measures for deep ae on test data
+#calculate and print Measures for deep ae on test data
 trust_ae_test = trustworthiness(Q_ae_test, min_k=1, max_k=25)
 cont_ae_test = continuity(Q_ae_test, min_k=1, max_k=25)
 print('Trustworthiness measure deep AE test data set{}'. format(trust_ae_test))
 print('Continuity measure deep AE test data set{}'. format(cont_ae_test))
 
-#calculate and print measures for denoised ae on train data
+#calculate and print Measures for denoised ae on train data
 trust_ae_denoised = trustworthiness(Q_ae_denoised, min_k=1, max_k=25)
 cont_ae_denoised = continuity(Q_ae_denoised, min_k=1, max_k=25)
 print('Trustworthiness measure denoised AE train data set{}'. format(trust_ae_denoised))
 print('Continuity measure denoised AE train data set{}'. format(cont_ae_denoised))
 
-#calculate and print measures for denoised ae on test data
+#calculate and print Measures for denoised ae on test data
 trust_ae_test_denoised = trustworthiness(Q_ae_test_denoised, min_k=1, max_k=25)
 cont_ae_test_denoised = continuity(Q_ae_test_denoised, min_k=1, max_k=25)
 print('Trustworthiness measure denoised AE test data set{}'. format(trust_ae_test_denoised))
 print('Continuity measure denoised AE test data set{}'. format(cont_ae_test_denoised))
 
-#plot measures for pca on train data
+#plot Measures for pca on train data
 plt.plot(cont_pca, "-m", label="continuity measure")
 plt.plot(trust_pca, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
@@ -148,7 +148,7 @@ plt.title('PCA on training data')
 plt.savefig('pca_train_data.png')
 plt.show()
 
-#plot measures for pca on test data
+#plot Measures for pca on test data
 plt.plot(cont_pca_test, "-m", label="continuity measure")
 plt.plot(trust_pca_test, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
@@ -158,7 +158,7 @@ plt.title('PCA on test data')
 plt.savefig('pca_test_data.png')
 plt.show()
 
-#plot measures for deep AE on train data
+#plot Measures for deep AE on train data
 plt.plot(cont_ae, "-m", label="continuity measure")
 plt.plot(trust_ae, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
@@ -168,7 +168,7 @@ plt.title('Deep Autoencoder on training data')
 plt.savefig('ae_train_data.png')
 plt.show()
 
-#plot measures for deep AE on test data
+#plot Measures for deep AE on test data
 plt.plot(cont_ae_test, "-m", label="continuity measure")
 plt.plot(trust_ae_test, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
@@ -178,7 +178,7 @@ plt.title('Deep Autoencoder on test data')
 plt.savefig('ae_test_data.png')
 plt.show()
 
-#plot measures for denoised AE on train data
+#plot Measures for denoised AE on train data
 plt.plot(cont_ae_denoised, "-m", label="continuity measure")
 plt.plot(trust_ae_denoised, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
@@ -188,7 +188,7 @@ plt.title('Denoising Deep Autoencoder on training data')
 plt.savefig('ae_train_data_denoised.png')
 plt.show()
 
-#plot measures for denoised AE on test data
+#plot Measures for denoised AE on test data
 plt.plot(cont_ae_test_denoised, "-m", label="continuity measure")
 plt.plot(trust_ae_test_denoised, "-c", label="trustworthiness measure")
 plt.legend(loc="upper right")
